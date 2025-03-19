e75d231df4c4f92d8ab4c50d6f2213d7986dbb32
Sreekaran1704 <86601811+Sreekaran1704@users.noreply.github.com>
2025-03-18 22:04:31 -0500

    Added Django environ variable

--- src/prac/settings.py
+++ src/prac/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-i_1fsk_%ua3nrqjso1b6nu4upn_19)h6qo2)0*say2f&ig*42@"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get("DEBUG")).lower() == "true"
+
+print("DEBUG",DEBUG, type(DEBUG))
 
 ALLOWED_HOSTS = [
     ".railway.app",

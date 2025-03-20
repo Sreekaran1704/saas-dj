617237531da21da2fff91ae37970dfb54ee9abda
Sreekaran1704 <86601811+Sreekaran1704@users.noreply.github.com>
2025-03-19 21:14:05 -0500

    Added DJANGO_DEBUG to docker file

--- src/prac/settings.py
+++ src/prac/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config("DJANGO_SECRET_KEY")
 #DEBUG = str(os.environ.get("DEBUG")).lower() == "true"
 DEBUG = config("DJANGO_DEBUG", cast=bool)
 
-print("DEBUG",DEBUG, type(DEBUG))
+#print("DEBUG",DEBUG, type(DEBUG))
 
 ALLOWED_HOSTS = [
     ".railway.app",
@@ -89,7 +89,7 @@ WSGI_APPLICATION = "prac.wsgi.application"
 
 
 CONN_MAX_AGE = config("CONN_MAX_AGE", default=30, cast=int)
-DATABASE_URL = config("DATABASE_URL",cast=str)
+DATABASE_URL = config("DATABASE_URL", default=None)
 
 if DATABASE_URL is not None:
     import dj_database_url
